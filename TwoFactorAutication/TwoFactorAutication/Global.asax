6d118a34c54890c71ca231e8d1224e4c238df508
SirMorsel <patrick.nibbia@gmail.com>
2018-09-17 08:50:44 +0200

    feat: add two_way_aut

---
+++ TwoFactorAutication/TwoFactorAutication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwoFactorAutication.MvcApplication" Language="C#" %>
---
+++ TwoFactorAutication/TwoFactorAutication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwoFactorAutication.Startup))]
+namespace TwoFactorAutication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
