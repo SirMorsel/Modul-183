cd93a09912528ef330e45a4cae0e90e484023047
PATRICK_LAPTOP\Patrick <patrick.nibbia@gmail.com>
2019-01-01 02:03:10 +0100

    feat: add cookie and Database_xss_sql_injection exercises

---
+++ Cookie/Cookie/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            var username = Request["username"];
+            var password = Request["password"];
+            var stayLoggedIn = Request["stayLoggedIn"];
+            var expireDate = -1;
+
+            if(stayLoggedIn == "on")
+            {
+                expireDate = 14;
+            }
+
+            if (username == "test" && password == "test")
+            {
+                string cookievalue;
+                if (Request.Cookies["cookie"] != null)
+                {
+                    cookievalue = Request.Cookies["cookie"].Value.ToString();
+                }
+                else
+                {
+                    Response.Cookies["cookie"].Value = "cookie value";
+                }
+                return View();
+            }
+
+            if (Request.Cookies["cookie"] != null)
+            {
+               Response.Cookies["cookie"].Expires = DateTime.Now.AddDays(expireDate);
+            }
+            return Login();
+        }
+    }
+}
---
+++ Database_xss_sql_injection/Database_xss_sql_injection/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace Database_xss_sql_injection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            dbRequest();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public void dbRequest()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\\C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf";
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+            cmd.CommandText = "SELECT [Id] ,[username] ,[password] FROM [dbo].[User]";
+            cmd.Connection = con;
+
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0}\t{1}", reader.GetInt32(0), reader.GetString(1));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+        }
+    }
+}
