c43faef2d6a5597476b2b670ae76e7ea4155a39f
PATRICK_LAPTOP\Patrick <patrick.nibbia@gmail.com>
2019-01-06 14:10:11 +0100

    feat: update tutorial 10

--- Database_xss_sql_injection/Database_xss_sql_injection/Controllers/HomeController.cs
+++ Database_xss_sql_injection/Database_xss_sql_injection/Controllers/HomeController.cs
@@ -15,16 +15,80 @@ namespace Database_xss_sql_injection.Controllers
             return View();
         }
 
-        public ActionResult About()
+        
+        public ActionResult Login()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult DoLogin()
         {
-            ViewBag.Message = "Your contact page.";
+            var username = Request["username"];
+            var password = Request["password"];
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\\C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf";
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT [Id], [username], [password] FROM [dbo].[User] WHERE [username] ='"+username+ "' AND [password] = '" + password;
+            cmd.Connection = con;
+
+            con.Open();
+
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                ViewBag.Message = "success";
+
+                while (reader.Read())
+                {
+                    ViewBag.Message += reader.GetInt32(0) + " " + reader.GetString(1) + " " + reader.GetString(2);
+                }
+            }
+            else
+            {
+                ViewBag.Message = "nothing to read of";
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Feedback()
+        {
+            var feedback = Request["feedback"];
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\\C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf";
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "INSERT INTO [dbo].[Feedback] SET [feedback] = '" + feedback + "'";
+            cmd.Connection = con;
+
+            con.Open();
+
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                ViewBag.Message = "success";
+
+                while (reader.Read())
+                {
+                    ViewBag.Message += reader.GetInt32(0) + " " + reader.GetString(1) + " " + reader.GetString(2);
+                }
+            }
+            else
+            {
+                ViewBag.Message = "nothing to read of";
+            }
 
             return View();
         }
