a32954437472600dd46a8cb2aecb0d221d014c73
PATRICK_LAPTOP\Patrick <patrick.nibbia@gmail.com>
2019-01-09 20:18:51 +0100

    fix: fix connection bug in tutorial 10

--- Database_xss_sql_injection/Database_xss_sql_injection/Controllers/HomeController.cs
+++ Database_xss_sql_injection/Database_xss_sql_injection/Controllers/HomeController.cs
@@ -15,14 +15,6 @@ namespace Database_xss_sql_injection.Controllers
             return View();
         }
 
-        
-        public ActionResult Login()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         [HttpPost]
         public ActionResult DoLogin()
         {
@@ -30,21 +22,21 @@ namespace Database_xss_sql_injection.Controllers
             var password = Request["password"];
 
             SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\\C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf";
-
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf;Integrated Security=True;Connect Timeout=30";
+            con.Open();
             SqlCommand cmd = new SqlCommand();
             SqlDataReader reader;
 
-            cmd.CommandText = "SELECT [Id], [username], [password] FROM [dbo].[User] WHERE [username] ='"+username+ "' AND [password] = '" + password;
+            cmd.CommandText = "SELECT [Id], [username], [password] FROM [dbo].[User] WHERE [username] ='"+username+ "' AND [password] = '" + password + "'";
             cmd.Connection = con;
 
-            con.Open();
+          //  con.Open();
 
             reader = cmd.ExecuteReader();
 
             if (reader.HasRows)
             {
-                ViewBag.Message = "success";
+                ViewBag.Message = "success: ";
 
                 while (reader.Read())
                 {
@@ -58,36 +50,32 @@ namespace Database_xss_sql_injection.Controllers
             return View();
         }
 
-        [HttpGet]
-        public ActionResult Feedback()
+        [HttpPost]
+        public ActionResult DoFeedback()
         {
             var feedback = Request["feedback"];
 
-            SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\\C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf";
-
-            SqlCommand cmd = new SqlCommand();
-            SqlDataReader reader;
-
-            cmd.CommandText = "INSERT INTO [dbo].[Feedback] SET [feedback] = '" + feedback + "'";
-            cmd.Connection = con;
-
-            con.Open();
-
-            reader = cmd.ExecuteReader();
-
-            if (reader.HasRows)
+            if (feedback != "")
             {
-                ViewBag.Message = "success";
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf;Integrated Security=True;Connect Timeout=30";
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO [dbo].[Feedback] (feedback) VALUES ('" + feedback + "')";
+                cmd.Connection = con;
 
-                while (reader.Read())
+                if (cmd.ExecuteNonQuery() != 0)
                 {
-                    ViewBag.Message += reader.GetInt32(0) + " " + reader.GetString(1) + " " + reader.GetString(2);
+                    ViewBag.Message = "success insert:" + feedback + " to DB!";
+                }
+                else
+                {
+                    ViewBag.Message = "nothing to read of";
                 }
             }
             else
             {
-                ViewBag.Message = "nothing to read of";
+                ViewBag.Message = "Do not let input-field empty";
             }
 
             return View();
@@ -96,8 +84,8 @@ namespace Database_xss_sql_injection.Controllers
         public void dbRequest()
         {
             SqlConnection con = new SqlConnection();
-            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\\C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf";
-
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Patrick\\Documents\\sql_xss_injection.mdf;Integrated Security=True;Connect Timeout=30";
+            con.Open();
             SqlCommand cmd = new SqlCommand();
             SqlDataReader reader;
             cmd.CommandText = "SELECT [Id] ,[username] ,[password] FROM [dbo].[User]";
---
+++ LoggingAuditTrailsDetection/LoggingAuditTrailsDetection/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen zu einer Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die mit einer Assembly verknüpft sind.
+[assembly: AssemblyTitle("LoggingAuditTrailsDetection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LoggingAuditTrailsDetection")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von "ComVisible" auf "false" werden die Typen in dieser Assembly unsichtbar
+// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly aus
+// COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der "typelib", wenn dieses Projekt für COM verfügbar gemacht wird.
+[assembly: Guid("27be4d6a-aa45-4f0d-a55d-b09ad65efa56")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern
+// übernehmen, indem Sie "*" eingeben:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
